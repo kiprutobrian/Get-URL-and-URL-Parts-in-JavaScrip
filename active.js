1348f3537ba03b85cd74d8bd86838e358bcbf6de
BRIAN KIPRUTO <briankipruto793@gmail.com>
2019-10-11 11:08:19 +0300

    Update active.js

--- active.js
+++ active.js
@@ -1,1 +1,22 @@
-Get URL and URL Parts in JavaScript
+function () {
+    let path1 = window.location.href
+    let path2 = window.location.href.split('/');
+    let path3 =  path_2[0]+'//'+ path_2[1]+path_2[2]+'/'+ path_2[3]
+
+    $('nav a').each(function () {
+        if (this.href == path_1) {
+            $(this).parent().addClass('active')
+
+            if ($(this).parent().closest('.subnav').length > 0) {
+                $(this).parent().closest('.subnav').addClass('active')
+            }
+        } else {
+            $(this).parent().removeClass('active')
+        }
+        if (this.href == path_3) {
+            $(this).parent().addClass('active')
+        } else {
+            $(this).parent().removeClass('active')
+        }
+    });
+};
